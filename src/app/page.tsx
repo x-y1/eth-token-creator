fb327a88535b3a8cea485436150e7eb650e6e32b
x-y1 <sodasosa21@gmail.com>
2024-09-06 09:35:58 -0700

    using materialui for layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Deployer from "./components/Deployer";
-
+import { Box, Container } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 export default function Home() {
   return (
-    <>
-      <div className="border border-white grid h-screen w-screen grid-rows-10">
-        <div className="border border-white row-start-1 row-span-1">
-          <Navbar/>
-        </div>
-        <div className="border border-white row-start-2 row-span-9">
+    <Container maxWidth={false} disableGutters className='border border-white h-screen'>
+      <Grid container direction='column' spacing={6} className='border border-white h-full'>
+        <Grid className='border border-red-800 h-[10%]'>
+        <Navbar/>
+        </Grid>
+        <Grid className='border border-red-800 flex-grow'>
           <Deployer/>
-        </div>
-      </div>
-    </>
+        </Grid>
+      </Grid>
+    </Container>
+   
   );
 }
