2451d111963d13a1b24eef27fe6d041bb59f4962
x-y1 <sodasosa21@gmail.com>
2024-09-23 15:09:49 -0700

    added custom scroll

--- src/app/components/ContractBox.tsx
+++ src/app/components/ContractBox.tsx
@@ -4,6 +4,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import '../scroll.css'
 export default function ContractBox() {
   const contract = useSelector((state: RootState) => state.contract.contract);
 
@@ -12,9 +13,9 @@ export default function ContractBox() {
       <Grid  className='h-[10%] pt-4'>
         Token Contract
       </Grid>
-      <Grid  size={{  }} className='w-5/6 h-5/6' >
+      <Grid  size={{  }} className='w-11/12 h-5/6' >
         <Box className='w-full h-full '>
-          <SyntaxHighlighter language="solidity" style={materialDark} className='w-full h-full '>
+          <SyntaxHighlighter language="solidity" style={materialDark} className='w-full h-full custom-scroll-container'>
             {contract}
           </SyntaxHighlighter>
         </Box>
