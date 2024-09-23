53890fd6edd6e68acb7e5b3d26aaab163c0309c2
x-y1 <sodasosa21@gmail.com>
2024-09-23 14:47:47 -0700

    added base contract

--- src/app/components/ContractBox.tsx
+++ src/app/components/ContractBox.tsx
@@ -2,14 +2,22 @@ import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 export default function ContractBox() {
+  const contract = useSelector((state: RootState) => state.contract.contract);
+
   return (
-    <Grid container spacing={4}  direction='column' className='h-full '>
-      <Grid container justifyContent='center' alignItems='center' className='h-[10%]'>
+    <Grid container direction='column' className='h-full ' alignItems='center'>
+      <Grid  className='h-[10%] pt-4'>
         Token Contract
       </Grid>
-      <Grid size={{xs: 'grow' }} className=''>
-
+      <Grid  size={{  }} className='w-5/6 h-5/6' >
+        <Box className='w-full h-full '>
+          <SyntaxHighlighter language="solidity" style={materialDark} className='w-full h-full '>
+            {contract}
+          </SyntaxHighlighter>
+        </Box>
       </Grid>
     </Grid>
   );
