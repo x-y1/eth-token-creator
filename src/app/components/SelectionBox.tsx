910f28c6f53b6d04e4f2c98f1cfbdc02c0a066c6
x-y1 <sodasosa21@gmail.com>
2024-09-24 10:17:55 -0700

    edited SelectFeatures layout

--- src/app/components/SelectionBox.tsx
+++ src/app/components/SelectionBox.tsx
@@ -1,16 +1,21 @@
 "use client"
 import Grid from "@mui/material/Grid2";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from "../store";
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { Checkbox, FormControlLabel, FormGroup, Divider } from "@mui/material";
+import { selectFeature } from "../store/featuresSlice";
 export default function SelectionBox() {
+    const dispatch = useDispatch();
     const features = useSelector((state: RootState) => state.features.features);
+    const handleCheck = (feature: string) => {
+        dispatch(selectFeature({ key: feature, value: !features[feature] }))
+    }
     return (
         <Grid container direction='column' spacing={4} className='h-full'>
             <Grid container justifyContent='center' alignItems='center' className='h-[10%]'>
                 Select Features
             </Grid>
-            <Grid size={{ xs: 'grow' }} className='p-8'>
+            <Grid size={{  }} className='p-8'>
                 <FormGroup>
                     {Object.keys(features).map((feature) => (
                         <FormControlLabel
@@ -19,12 +24,17 @@ export default function SelectionBox() {
                             control={
                                 <Checkbox
                                     checked={features[feature]}
+                                    onChange={() => handleCheck(feature)}
                                     color="primary"
                                 />
                             }
                         />
                     ))}
                 </FormGroup>
+            </Grid>
+            <Divider variant="fullWidth" className="opacity-100 bg-black"/>
+            <Grid>
+                Info
             </Grid>
         </Grid>
     );
